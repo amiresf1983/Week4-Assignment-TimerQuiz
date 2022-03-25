a556fb36e8b5ce9b0ae6c1e0f5832ac670ed03d3
Amir Esfandiari <99114431+amiresf1983@users.noreply.github.com>
2022-03-25 22:53:26 +1100

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,32 @@
+let questions = [
+  {
+    numb: 1,
+    question: "Where is the capital of Finland?",
+    answer: "Helsinky",
+    options: ["Berlin", "Helsinky", "Paris", "Oslow"],
+  },
+  {
+    numb: 2,
+    question: "Which country is not a neighbor of Russia?",
+    answer: "Poland",
+    options: ["Poland", "Litvania", "Ukraine", "Belarus"],
+  },
+  {
+    numb: 3,
+    question: "which country is not an island?",
+    answer: "Greece",
+    options: ["England", "Iceland", "Ireland", "Greece"],
+  },
+  {
+    numb: 4,
+    question: "Which Sea is not located in Europe?",
+    answer: "Tasman Sea",
+    options: ["Black Sea", "Mediteranian Sea", "Baltic Sea", "Tasman Sea"],
+  },
+  {
+    numb: 5,
+    question: "Which country is more populated?",
+    answer: "Germany",
+    options: ["Norway", "Iran", "Germany", "Spain"],
+  },
+];
